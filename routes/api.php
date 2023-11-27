4e022bb531ceab52577bd74628a6f28950273587
Fabio Lisboa <lisboadivalu@gmail.com>
2023-11-27 12:51:25 -0300

    feat: routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EmailController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'auth'], function (){
+    Route::post('login', [AuthController::class, 'login']);
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+});
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::group([ 'prefix' => 'email'], function(){
+        Route::post('create', [EmailController::class, 'store'])->withoutMiddleware('auth:api');
+        Route::post('send', [EmailController::class, 'sendEmail']);
+        Route::group(['prefix' => 'get'], function (){
+            Route::get('all', [EmailController::class, 'getAll']);
+        });
+    });
+
+    Route::group(['prefix' => 'user'], function() {
+        Route::post('create', [UserController::class, 'store']);
+    });
+});
+
+
+
